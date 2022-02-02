ca0024a6ccd733570051c84e118d258fba20a169
pae-panutat <panutatinfo@gmail.com>
2022-02-02 15:41:03 +0700

    02-02-65

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\task;
+use Faker\Generator as Faker;
+
+$factory->define(task::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ resources/views/main.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.mainlayout')
+@section('content')
+
+    <main class="container">
+        <div class="bg-light p-5 rounded">
+            <h1>main</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores quidem soluta iure, quibusdam culpa illum rerum sapiente cupiditate harum. Quod fugit dolorum nemo dolorem velit quibusdam est dolore repudiandae vel.</p>
+        </div>
+    </main>
+
+@endsection
